db57df4ee4d64f679465ede718337a47a6906f5b
yaminichintada2005-ui <yaminichintada2005@gmail.com>
2026-03-13 23:44:09 +0530

    Create Scramble string.java

---
+++ Scramble string.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+class Solution {
+
+    Map<String, Boolean> memo = new HashMap<>();
+
+    public boolean isScramble(String s1, String s2) {
+
+        if(s1.equals(s2))
+            return true;
+
+        String key = s1 + "#" + s2;
+
+        if(memo.containsKey(key))
+            return memo.get(key);
+
+        if(!isSameChar(s1, s2)){
+            memo.put(key, false);
+            return false;
+        }
+
+        int n = s1.length();
+
+        for(int i = 1; i < n; i++){
+
+            // no swap
+            if(isScramble(s1.substring(0, i), s2.substring(0, i)) &&
+               isScramble(s1.substring(i), s2.substring(i))){
+                memo.put(key, true);
+                return true;
+            }
+
+            // swap
+            if(isScramble(s1.substring(0, i), s2.substring(n-i)) &&
+               isScramble(s1.substring(i), s2.substring(0, n-i))){
+                memo.put(key, true);
+                return true;
+            }
+        }
+
+        memo.put(key, false);
+        return false;
+    }
+
+    private boolean isSameChar(String a, String b){
+
+        int[] count = new int[26];
+
+        for(char c : a.toCharArray())
+            count[c - 'a']++;
+
+        for(char c : b.toCharArray())
+            count[c - 'a']--;
+
+        for(int x : count)
+            if(x != 0)
+                return false;
+
+        return true;
+    }
+}
